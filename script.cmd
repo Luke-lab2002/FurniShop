61d04bc371935ad98763e837c62989d47ce6bf92
Luke-lab2002 <nguyenvuminhcuong2002@gmail.com>
2024-10-31 06:05:48 +0700

    created-relationship

--- script.cmd
+++ script.cmd
@@ -1,9 +1,9 @@
 npx sequelize-cli model:generate --name Users --attributes name:string,email:string,password:string,ban:boolean
 npx sequelize-cli model:generate --name Orders --attributes user_id:integer,address:string,state_id:integer,state_notifi:boolean
 npx sequelize-cli model:generate --name Admin --attributes name:string,email:string,password:string
-npx sequelize-cli model:generate --name Products --attributes name:string,price:integer,url_image:string,code:string,id_admin:integer
+npx sequelize-cli model:generate --name Products --attributes name:string,price:integer,url_image:string,code:string,admin_id:integer
 npx sequelize-cli model:generate --name OrderDetails --attributes order_id:integer,product_id:integer,number:integer
-npx sequelize-cli model:generate --name Articles --attributes title:string,content:text,url_image:string,id_admin:integer
+npx sequelize-cli model:generate --name Articles --attributes title:string,content:text,url_image:string,admin_id:integer
 
 npx sequelize-cli model:generate --name CommentArticle --attributes user_id:integer,content:text,article_id:integer,state_notifi:boolean
 npx sequelize-cli model:generate --name CommentProducts --attributes user_id:integer,content:text,product_id:integer,state_notifi:boolean
